d9f9bc9b619148e93f9e3b69960a827a4c201753
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-02-10 00:40:34 +0530

    feat[SHADCN]: added shadcn package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@bitwork/ui/components/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="font-bold text-4xl">Welcome to Bitwork!</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
